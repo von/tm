8a8973cb12fb391bbee0d7ae4a908057cf6778bf
Von Welch <von@vwelch.com>
2013-04-23 12:53:41 -0400

    tm-init.sh: Fix for source init files.

--- tm-init.sh
+++ tm-init.sh
@@ -17,17 +17,6 @@
 if test -n "${TMUX}" ; then
     # We are in tmux...
 
-    # Only do tmux initialization once (not again if bashrc is resourced)
-    if test -z "${TMUX_INIT_COMPLETE}" ; then
-        TMUX_INIT_COMPLETE=1
-
-        TMUX_INIT_PATH=${HOME}/.tmux/init/
-        TMUX_SESSION_INIT_FILE=${TMUX_INIT_PATH}/${TMUX_SESSION_NAME}
-        if test -e "${TMUX_SESSION_INIT_FILE}" ; then
-            source ${TMUX_SESSION_INIT_FILE}
-        fi
-    fi
-
     # Print name of tmux session
     tmux_session_name () {
         # This handles multiple panes in a window and running in a pane
@@ -56,4 +45,15 @@ if test -n "${TMUX}" ; then
     tmux_set_pane_title() {
       printf "\033]2;${*}\033\\"
     }
+
+    # Only do tmux initialization once (not again if bashrc is resourced)
+    if test -z "${TMUX_INIT_COMPLETE}" ; then
+        TMUX_INIT_COMPLETE=1
+
+        TMUX_INIT_PATH=${HOME}/.tmux/init/
+        TMUX_SESSION_INIT_FILE=${TMUX_INIT_PATH}/$(tmux_session_name)
+        if test -e "${TMUX_SESSION_INIT_FILE}" ; then
+            source ${TMUX_SESSION_INIT_FILE}
+        fi
+    fi
 fi
